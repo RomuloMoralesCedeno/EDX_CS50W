90a6a2fd831ae060d343fe17b7a7e48a8ac2da96
unknown <morales.romulo@gmail.com>
2020-08-03 10:05:07 -0600

    first commit

---
+++ loops.py
@@ -0,0 +1,5 @@
+for i in [0,1,2,3,4]:
+    print(i)
+#range
+for i in range(10):
+    print(i)
---
+++ name.py
@@ -0,0 +1,3 @@
+name = input("Please type your name:")
+print("Hello " + name)
+print(f"hello, {name}")
